9065ae128fcb8370b27a03df18251b6e59c358d7
Richard2203 <55164319+Richard2203@users.noreply.github.com>
2021-07-13 23:55:08 -0500

    aniadiendo estilos css a un form

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -2,9 +2,42 @@ import React from 'react';
 
 const LoginScreen = () => {
 	return (
-		<div>
-			<h2>Login Screen</h2>
-		</div>
+		<>
+			<h3>Login</h3>
+			<form>
+				<div className="form-control">
+					<input
+						type="text"
+						id="email"
+						className="form-control__input"
+						autoComplete="off"
+						placeholder=" "
+					/>
+
+					<label htmlFor="email" className="form-control__label">
+						Email
+					</label>
+				</div>
+
+				<div className="form-control">
+					<input
+						type="password"
+						id="password"
+						className="form-control__input"
+						autoComplete="off"
+						placeholder=" "
+					/>
+
+					<label htmlFor="password" className="form-control__label">
+						password
+					</label>
+				</div>
+
+				<button type="submit" className="btn cursor">
+					Login
+				</button>
+			</form>
+		</>
 	);
 };
 
--- src/routers/AuthRouter.jsx
+++ src/routers/AuthRouter.jsx
@@ -5,18 +5,24 @@ import RegisterScreen from '../components/auth/RegisterScreen';
 
 const AuthRouter = () => {
 	return (
-		<div>
-			<Switch>
-				{/* el path debe ser exacto para poderse mostrar
+		<div className="auth__main">
+			<div className="auth__box-container">
+				<Switch>
+					{/* el path debe ser exacto para poderse mostrar
                 el componente correspondiente a su path, no habra
                 variaciones de path para rutas hijas */}
 
-				{/* colocamos el "/auth" para indicar que es ruta hija del 
+					{/* colocamos el "/auth" para indicar que es ruta hija del 
                 mismo */}
-				<Route exact path="/auth/login" component={LoginScreen} />
-				<Route exact path="/auth/register" component={RegisterScreen} />
-				<Redirect to="/auth/register" />
-			</Switch>
+					<Route exact path="/auth/login" component={LoginScreen} />
+					<Route
+						exact
+						path="/auth/register"
+						component={RegisterScreen}
+					/>
+					<Redirect to="/auth/register" />
+				</Switch>
+			</div>
 		</div>
 	);
 };
