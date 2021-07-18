0911190d7db27911d6d30abc0ed299b24466c497
Richard2203 <55164319+Richard2203@users.noreply.github.com>
2021-07-18 00:10:09 -0500

    implementando muestra de notas en sidebar

--- src/components/journal/JournalEntry.jsx
+++ src/components/journal/JournalEntry.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
+// esta libreria nos permite la manipulacion de fechas
+// se instala con el comando
+// 	npm install moment --save
+import moment from 'moment';
 
-const JournalEntry = () => {
+const JournalEntry = ({ id, date, title, body, url }) => {
+	const dateMoment = moment(date);
+
 	return (
 		<div className="journal__entry cursor">
 			{/* en React la etiqueta style se maneja pasando las propieades
             como si de un objeto se tratara */}
-			<div
-				className="journal__entry-picture"
-				style={{
-					backgroundSize: 'cover',
-					backgroundImage:
-						'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaADuQ1v9s3kWzKvKTsqEGVIwc_VqKAIdfew&usqp=CAU)',
-				}}
-			></div>
-
+			{url && (
+				<div
+					className="journal__entry-picture"
+					style={{
+						backgroundSize: 'cover',
+						backgroundImage: `url(${url})`,
+					}}
+				></div>
+			)}
 			<div className="journal__entry-body">
-				<p className="journal__entry-title">a new day</p>
-				<p className="journal__entry-content">
-					Today read a new big book and very interesting
-				</p>
+				<p className="journal__entry-title">{title}</p>
+				<p className="journal__entry-content">{body}</p>
 			</div>
 
 			<div className="journal__entry-date">
-				<span>Monday</span>
-				<h4>24</h4>
+				{/* el formato de las fechas se optiene de la documentacion de 
+				https://momentjs.com/ */}
+				<span>{dateMoment.format('dddd')}</span>
+				<h4>{dateMoment.format('Do')}</h4>
 			</div>
 		</div>
 	);
