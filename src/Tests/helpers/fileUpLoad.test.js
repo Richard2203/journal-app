91f1658b27eac88e4b12017470c25cefeee1b55d
Richard2203 <55164319+Richard2203@users.noreply.github.com>
2021-07-20 19:10:44 -0500

    aplicando pruebas en el helper fileUpLoad

--- src/Tests/helpers/fileUpLoad.test.js
+++ src/Tests/helpers/fileUpLoad.test.js
@@ -1,4 +1,21 @@
+// el SDK de cloudinary nos ofrece funciones y procesos adicionales,
+// entre ellos el borrado de imagenes. Para poder usarlo se debe de instalar
+// la siguiente dependecia; se emplea "--sav-dev" para indicar que es dependencia
+// de desarrollo
+//      npm install cloudinary --save-dev
+// DOCUMENTACION EN NODE:
+//      https://cloudinary.com/documentation/node_integration
+import cloudinary from 'cloudinary';
+
 import { fileUpLoad } from '../../helpers/fileUpLoad';
+
+// los valores salen de la pagina de Cloudinary en la seccion de Dashboard
+cloudinary.config({
+	cloud_name: 'djrygievc',
+	api_key: '123917899154161',
+	api_secret: 'jadGdhXIBg3GMEXBF3tXwhIILRA',
+	secure: true,
+});
 
 describe('Pruebas en el helper fileUpLoad', () => {
 	test('debe de cargar un archivo y retornar el URL', async () => {
@@ -21,6 +38,17 @@ describe('Pruebas en el helper fileUpLoad', () => {
 		const url = await fileUpLoad(file);
 
 		expect(typeof url).toBe('string');
+
+		// obteniendo el ID de la imagen
+		const segments = url.split('/');
+		const imageId = segments[segments.length - 1].replace('.png', '');
+
+		// Borrado de la imagen
+		//  -primer argumento es un arreglo con los ids de las imagenes a borrar
+		//  -segundo argumento debe ser {} en este caso
+		//  -el tercer argumento es una funcion a ejecutar al finalizar el proceso
+
+		cloudinary.v2.api.delete_resources([imageId], {}, () => {});
 	});
 
 	test('debe retornar null como error', async () => {
